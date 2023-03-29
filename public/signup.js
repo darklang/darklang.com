8beb6c214eb139030123a978b0e2c6f568fdd8a0
Bouteldja feriel <feriel@Bouteldjas-MacBook-Pro.local>
2023-03-29 14:39:36 +0200

    Remove sign up success message creation from JS

--- public/signup.js
+++ public/signup.js
@@ -19,50 +19,12 @@ function showErrors(errors) {
 }
 
 function showSuccess() {
-    const form = document.querySelector("#sign-up-form");
-
-    // remove all but first thing (which is the title)
-    const firstElementChild = form.firstElementChild;
+    const form = document.getElementById("sign-up-form");
+    const successMsg = document.getElementById("success")
     form.innerHTML = '';
-    form.append(firstElementChild);
-    form.classList.add("flex", "flex-col", "items-center", "text-center", "justify-center");
-
-    let successDiv = document.createElement("div");
-
-    // create the success icon element
-    let successIcon = document.createElement("i");
-    successIcon.classList.add("fas", "fa-check-circle", "text-pink", "mt-11", "text-6xl");
-
-    successDiv.appendChild(successIcon);
-
-    // create a div element to wrap the success text and login link
-    let successContent = document.createElement("div");
-
-    // add the success text and login link to the successContent div
-    let successText = document.createElement("p");
-    successText.classList.add("text-pink", "font-medium", "text-2xl", "mt-9");
-    successText.appendChild(document.createTextNode("Success! "));
-
-    let successText2 = document.createElement("p");
-    successText2.classList.add("text-white", "font-medium", "text-xl", "mt-2");
-    successText2.appendChild(document.createTextNode("Congratulations! Your sign up was successful"));
-
-    let logInLink = document.createElement("a");
-    logInLink.classList.add("px-16", "py-2", "text-lg", "rounded", "bg-pink", "text-white", "hover:bg-background", "hover:border", "hover:border-pink", "transition", "duration-200", "ease-in-out", "mt-12");
-    logInLink.href = "https://login.darklang.com";
-    logInLink.appendChild(document.createTextNode("Log in"));
-
-    successContent.appendChild(successText);
-    successContent.appendChild(successText2);
-    successContent.appendChild(logInLink);
-
-    // apply Flexbox properties to the successContent div
-    successContent.classList.add("flex", "flex-col", "items-center", "text-center", "justify-center");
-
-    successDiv.appendChild(successContent);
-    successDiv.classList.add("flex", "flex-col", "items-center", "text-center", "justify-center", "px-12");
-
-    form.appendChild(successDiv);
+    successMsg.classList.remove("hidden");
+    successMsg.classList.add("flex")
+    form.append(successMsg);    
 }
 
 async function submitForm(event) {
@@ -96,6 +58,8 @@ async function submitForm(event) {
         },
         body: JSON.stringify(data),
     });
+    // showSuccess();
+    // showErrors("error test")
 
     if (response.status === 200) {
         form.reset()

5c1cea448f7637b1d970440e6a82b8e1b5b922ad
Ocean <oakocean9@gmail.com>
2023-10-16 19:26:26 +0200

    Delete most of the old homepage

--- tailwind.config.js
+++ tailwind.config.js
@@ -15,22 +15,6 @@ module.exports = {
       body: ['Inter', "sans-serif"],
     },
     extend: {
-      colors: {
-        'pink': '#F56FF0',
-        'purple': '#939BFF',
-        'background': '#050407',
-        'magenta': '#C56AE4',
-      },
-      keyframes: {
-        fill: {
-          '0%':{ transform: 'scale(0.8)'},
-          '100%':{transform: 'scale(1)'},
-      },
-      },
-      animation: {
-        fill: 'fill ease-out 0.4s',
-
-      }
     },
   },
   plugins: [],

bcd5b9dc90dd0f83aad185ff0ca1422a7407dce3
Ocean <oakocean9@gmail.com>
2023-10-17 09:48:45 +0200

    WIP new homepage

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,19 +2,27 @@
 module.exports = {
   content: ["./public/*.{html,js}"],
   theme: {
-    screens : {
-      'xxs': '320px',
-      'xs': '375px',
-      'sm': '640px',
-      'md': '768px',
-      'lg': '1024px',
-      'xl': '1280px',
-      '2xl': '1536px',
-    },
     fontFamily: {
       body: ['Inter', "sans-serif"],
+      'League-Gothic': ['League Gothic', 'sans-serif'],
+      'Assistant': ['Assistant', 'sans-serif'],
+    },
+    listStyleType: {
+      none: 'none',
+      disc: 'disc',
+      decimal: 'decimal',
+      square: 'square',
+      roman: 'upper-roman',
     },
     extend: {
+      colors: {
+        'light-background': '#F1F1F1',
+        'purple': '#955B9F',
+        'blue': '#747AB9',
+      },
+      flex: {
+        '2': '2 2 0%'
+      }
     },
   },
   plugins: [],
